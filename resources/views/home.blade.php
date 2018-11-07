f760fa02e8720b7cdf84c75f472ec6ed5b34ae33
Shun-Ge <qaz581121@163.com>
2018-11-07 06:04:57 +0000

    Finish layout and routes

--- resources/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.default')
-@section('title', '主页')
-
-@section('content')
-  <h1>主页</h1>
-@stop
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample')</title>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </body>
 </html>
