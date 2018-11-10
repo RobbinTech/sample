1b71a3c2f8e443b0dd5e39511285e9d517c765e6
Shun-Ge <qaz581121@163.com>
2018-11-10 07:51:48 +0000

    完成用户模块：编辑，更新，删除，首页

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,24 @@ use App\Models\User;
 use Auth;
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            //except 除了此处指定的动作以外，所有其他动作都必须登录用户才能访问
+            'except' => ['index', 'show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
@@ -33,6 +51,39 @@ class UsersController extends Controller
         ]);
         Auth::login($user); //设置注册成功自动登陆
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程(ง •_•)ง');
-        return redirect()->route('users.show', [$user]);
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password){
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功(ง •_•)ง');
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
